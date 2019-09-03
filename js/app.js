c1ef4024510c7c69d2fe84c17f6b08ce30c711b3
Jedediah Arnold <jarnold910@gmail.com>
2019-09-03 14:57:29 -0400

    winner modal added, not working yet

--- js/app.js
+++ js/app.js
@@ -23,6 +23,7 @@ let cards = [
 
 let moveCounter = 0;
 let moves = document.querySelector(".moves");
+let messageContent = document.querySelector(".winner");
 
 function initGame() {
   let deck = document.querySelector(".deck");
@@ -66,6 +67,33 @@ function shuffle(array) {
 
   return array;
 }
+
+// Modal Popup Logic
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
 
 /*
  * set up the event listener for a card. If a card is clicked:
@@ -113,6 +141,8 @@ allCards.forEach(function(card) {
         moveCounter++;
       }
       moves.innerText = moveCounter;
+      messageContent.innerText = `Congratulations!
+      You won in ${moveCounter} moves!`;
     }
   });
 });
